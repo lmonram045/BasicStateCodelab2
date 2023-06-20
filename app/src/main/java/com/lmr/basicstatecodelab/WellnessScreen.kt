387317a5b78f33da1d2827b5d6bac13ab8d57a11
Luis <lmr.87@protonmail.ch>
2023-06-20 22:47:24 +0200

    Punto 5

---
+++ app/src/main/java/com/lmr/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,11 @@
+package com.lmr.basicstatecodelab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
